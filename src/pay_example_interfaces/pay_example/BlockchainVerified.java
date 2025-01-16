0a0f7041a7ac8c439907d6dfd05623ba2cfbeb9c
conalnwrc <conal.grumley@nwrc.ac.uk>
2025-01-16 12:22:22 +0000

    Delete src/pay_example_interfaces/pay_example directory

--- src/pay_example_interfaces/pay_example/BlockchainVerified.java
+++
@@ -1,5 +0,0 @@
-package pay_example_interfaces.pay_example;
-
-public interface BlockchainVerified {
-    void verifyTransaction(String transactionId);
-}
--- src/pay_example_interfaces/pay_example/PaymentMethod.java
+++
@@ -1,6 +0,0 @@
-package pay_example_interfaces.pay_example;
-
-public interface PaymentMethod {
-    void processPayment(double amount);
-    void refundPayment(double amount);
-}
